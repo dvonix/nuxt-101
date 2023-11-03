acd7a166b9feee03560cc6285c42abba3052768a
dvonix <fakhrialbari.pro@gmail.com>
2023-11-04 06:11:39 +0700

    add pages

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque ipsam, officiis suscipit commodi, reprehenderit obcaecati repellendus debitis quasi officia magnam incidunt quas natus cumque numquam sequi ea assumenda cupiditate quod.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus enim illum eligendi deleniti, dolore quaerat suscipit fugit omnis voluptatem minus, ipsam quisquam neque ex alias, iure aspernatur sapiente! Minima, natus!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>This is product!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
